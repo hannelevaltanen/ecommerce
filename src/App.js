eb9d1d20d0693e68add90dd15275d516b2a2486a
Hannele Valtanen <hannele.valtanen@teamit.fi>
2020-05-09 13:14:44 +0300

    Setup Redux

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {

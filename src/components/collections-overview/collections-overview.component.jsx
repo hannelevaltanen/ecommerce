663e3fdfc9e839717bd870afa4532bec6609d876
Hannele Valtanen <hannele.valtanen@teamit.fi>
2021-02-05 18:16:25 +0200

    Data Flow In App

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -4,7 +4,7 @@ import { createStructuredSelector } from 'reselect';
 
 import CollectionPreview from '../collection-preview/collection-preview.component';
 
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
 import './collections-overview.styles.scss';
 
@@ -15,7 +15,7 @@ const collectionsOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-	collections: selectCollections
+	collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(collectionsOverview);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -6,6 +6,11 @@ const selectShop = state => state.shop;
 export const selectCollections = createSelector([ selectShop ], shop => shop.collections);
 
 
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
+);
+
 export const selectCollection = memoize((collectionUrlParam) =>
     createSelector(
         [selectCollections],

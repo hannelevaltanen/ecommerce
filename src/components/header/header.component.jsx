c08cadb67c990969786b2363a47ac0a437b3d5a1
Hannele Valtanen <hannele.valtanen@teamit.fi>
2021-02-21 14:32:17 +0200

    Add styled components, refactor homepage and header components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
@@ -11,34 +10,39 @@ import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
-import './header.styles.scss';
+import {
+	HeaderContainer,
+	LogoContainer,
+	OptionsContainer,
+	OptionLink
+} from './header.styles';
 
 const Header = ({ currentUser, hidden }) => (
-	<div className="header">
-		<Link className="logo-container" to="/">
+	<HeaderContainer>
+		<LogoContainer to="/">
 			<Logo className="logo" />
-		</Link>
+		</LogoContainer>
 
-		<div className="options">
-			<Link className="option" to="/shop">
+		<OptionsContainer>
+			<OptionLink to="/shop">
 				Shop
-			</Link>
-			<Link className="option" to="/contact">
+			</OptionLink>
+			<OptionLink to="/contact">
 				Contact
-			</Link>
+			</OptionLink>
 			{currentUser ? (
-				<div className="option" onClick={() => auth.signOut()}>
+				<OptionLink as='div' onClick={() => auth.signOut()}>
 					Sign out
-				</div>
+				</OptionLink>
 			) : (
-				<Link className="option" to="/signin">
+				<OptionLink to="/signin">
 					Sign in
-				</Link>
+				</OptionLink>
 			)}
 			<CartIcon />
-		</div>
+		</OptionsContainer>
 		{hidden ? null : <CartDropdown />}
-	</div>
+	</HeaderContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
